90fce63f2113eea3f80bdb79debe2db28a5a133b
ericmartineau <smartytime@gmail.com>
2022-05-02 09:42:14 -0700

    feat: bumping version

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ericm/fvm/versions/2.2.1"
+export "FLUTTER_ROOT=/Users/ericm/fvm/versions/2.10.5"
 export "FLUTTER_APPLICATION_PATH=/Users/ericm/sunny/local_plugins/fab_circular_menu/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
